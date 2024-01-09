a6b2bf01f7e8277015580863d6fbbaeaf27e33e6
ilya885400 <ilya885400@mail.ru>
2024-01-09 18:28:10 +1000

    Register

--- Controller/auth.py
+++ Controller/auth.py
@@ -7,27 +7,45 @@ from app import db
 from forms import RegistrationForm, LoginForm
 
 auth = Blueprint('auth', __name__)
+from flask import render_template
+from Model.models import Region
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        firstname = form.firstname.data
-        lastname = form.lastname.data
-        age = form.age.data
-        region = form.region.data
-        email = form.email.data
-        userdb = User.query.filter_by(email=email).first()
-        if userdb is not None and userdb.email == email:
-            flash("Пользователь с таким email уже существует")
-            return redirect(url_for('auth.register'))
-        user = User(firstname = firstname, lastname = lastname, age = age, region = region, email = email)
-        user.password = form.password.data
 
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        return redirect(url_for('CZN.index'))
+    form.region.choices = [(region.id, region.region) for region in Region.query.all()]
+
+    try:
+        if form.validate_on_submit():
+            firstname = form.firstname.data
+            lastname = form.lastname.data
+            age = form.age.data
+            region_id = form.region.data
+            email = form.email.data
+            phone_number = form.phone_number.data
+            employer_status = form.employer_status.data
+
+
+            userdb = User.query.filter_by(email=email).first()
+
+            if userdb is not None and userdb.email == email:
+                flash("Пользователь с таким email уже существует")
+                return redirect(url_for('auth.register'))
+
+            user = User(firstname=firstname, lastname=lastname, age=age, region_id=region_id, email=email,
+                        phone_number=phone_number, employer_status=employer_status)
+            user.set_password(form.password.data)
+
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            return redirect(url_for('CZN.index'))
+    except Exception as e:
+        print(f"An error occurred in register view: {str(e)}")
+        raise
+
+    print(form.errors)
     return render_template('register.html', form=form)
 
 
